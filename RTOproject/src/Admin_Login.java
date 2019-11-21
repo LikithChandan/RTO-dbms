d23e20d0d37da492844a5abad372ec12ce38af9a
LikithChandan <56481132+LikithChandan@users.noreply.github.com>
2019-11-21 15:08:58 +0530

    added graphics to adminlogin page

--- RTOproject/src/Admin_Login.java
+++ RTOproject/src/Admin_Login.java
@@ -42,37 +42,41 @@ public class Admin_Login
 	{
 		frame = new JFrame();
 		frame.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 15));
-		frame.setBounds(100, 100, 459, 328);
+		frame.setBounds(100, 100, 1200, 733);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		c=frame.getContentPane();
 		
 		
 		JLabel lblWelcomeToRto = new JLabel("WELCOME TO RTO MANAGEMENT SYSTEM");
-		lblWelcomeToRto.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblWelcomeToRto.setBounds(31, 11, 402, 40);
+		lblWelcomeToRto.setFont(new Font("Bodoni MT Black", Font.BOLD, 38));
+		lblWelcomeToRto.setBounds(103, 13, 979, 93);
 		c.add(lblWelcomeToRto);
 		
 		JLabel lblAdminId = new JLabel("Admin ID :");
-		lblAdminId.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblAdminId.setBounds(133, 117, 80, 21);
+		lblAdminId.setFont(new Font("Sylfaen", Font.BOLD, 24));
+		lblAdminId.setBounds(362, 177, 147, 49);
 		frame.getContentPane().add(lblAdminId);
 		
 		textField = new JTextField();
-		textField.setBounds(213, 119, 86, 20);
+		textField.setFont(new Font("Sylfaen", Font.PLAIN, 24));
+		textField.setBounds(521, 178, 240, 49);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblPassword = new JLabel("Password :");
-		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblPassword.setBounds(130, 149, 83, 14);
+		lblPassword.setFont(new Font("Sylfaen", Font.BOLD, 24));
+		lblPassword.setBounds(362, 273, 147, 54);
 		frame.getContentPane().add(lblPassword);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(213, 148, 86, 20);
+		passwordField.setFont(new Font("Sylfaen", Font.PLAIN, 24));
+		passwordField.setBounds(521, 277, 240, 48);
 		frame.getContentPane().add(passwordField);
 		
 		JButton btnLogin = new JButton("Login");
+		btnLogin.setBackground(Color.DARK_GRAY);
+		btnLogin.setForeground(Color.BLACK);
 		btnLogin.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e)
@@ -110,9 +114,14 @@ public class Admin_Login
 				}
 			}
 		});
-		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnLogin.setBounds(178, 189, 70, 23);
+		btnLogin.setFont(new Font("Sylfaen", Font.BOLD, 24));
+		btnLogin.setBounds(550, 388, 110, 42);
 		frame.getContentPane().add(btnLogin);
-		//noob
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setForeground(Color.BLACK);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\LC\\Desktop\\RTO project design\\pic1.jpg"));
+		lblNewLabel.setBounds(0, 0, 1182, 686);
+		frame.getContentPane().add(lblNewLabel);
 	}
 }
